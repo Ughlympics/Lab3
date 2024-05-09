f99455544418f62a2fde487e80386f88ecbf2398
Ughlympics <kipgora@gmail.com>
2024-05-09 22:54:33 +0300

    Last hope

--- garbage_test.lua
+++ garbage_test.lua
@@ -1,19 +1,35 @@
 local busted = require("busted")
 local assert = require("luassert")
 
+describe("garbage collection", function()
 
-describe("Garbage Test", function()
-    it("Checks the operation of the garbage collector", function()
-        local objects = {}
-        for i = 1, 100000 do
-            table.insert(objects, {})
-        end
-        for i = #objects, 1, -1 do
-            table.remove(objects, i)
-        end
-        local first = collectgarbage('count')
-        collectgarbage()
-        assert.is_true(collectgarbage('count') < first)
-        
-    end)
+    it("numbers should not affect collected memory", function()
+        collectgarbage('collect');
+        collectgarbage('collect');
+        collectgarbage('collect');
+
+        local v = 5
+
+        local memory_usage_before = collectgarbage('count')
+        v = nil
+        collectgarbage('collect');
+        local memory_usage_after = collectgarbage('count')
+
+        assert.equal(memory_usage_before, memory_usage_after)
+   end)
+
+   it("tables without references should be collected", function()
+        collectgarbage('collect');
+        collectgarbage('collect');
+        collectgarbage('collect');
+
+        local v = {}
+
+        local memory_usage_before = collectgarbage('count')
+        v = nil
+        collectgarbage('collect');
+        local memory_usage_after = collectgarbage('count')
+
+        assert.truthy(memory_usage_after < memory_usage_before)
+   end)
 end)
