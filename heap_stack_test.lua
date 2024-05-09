8dba3bae87da89cd49ba6259f27d200d3637e614
Ughlympics <kipgora@gmail.com>
2024-05-09 22:35:25 +0300

    Fixed

--- heap_stack_test.lua
+++ heap_stack_test.lua
@@ -1,22 +1,24 @@
 local busted = require("busted")
 local assert = require("luassert")
 
-describe("Stack test", function()
-    it("We create objects in stack ", function()
-       collectgarbage()
-       local heap = 9
-       heap = nil
-       
-        for i = 1, 10 do
-            local memory = collectgarbage('count')
-        end
-        collectgarbage()
-        for i = 1, 10 do
-            local memory2 = collectgarbage('count')
-        end
-       
-       
-       assert.equal(memory2, memory)
+describe("value types", function()
+    it("numbers allocation should not increase heap", function()
+        local memory_usage_before = collectgarbage("count")
+        local v = 5
+        v = nil
+        local memory_usage_after = collectgarbage("count")
 
-    end)
+        assert.truthy(memory_usage_before == memory_usage_after)
+   end)
+end)
+
+describe("reference types", function()
+    it("table allocation should increase heap", function()
+        local memory_usage_before = collectgarbage("count")
+        local v = {}
+        v = nil
+        local memory_usage_after = collectgarbage("count")
+
+        assert.truthy(memory_usage_after > memory_usage_before)
+   end)
 end)
