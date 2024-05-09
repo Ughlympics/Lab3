c30b2d4d1e33ff6c282e88b5ec994738a911c0aa
Ughlympics <kipgora@gmail.com>
2024-05-09 19:16:32 +0300

    Heap fix

--- heap_stack_test.lua
+++ heap_stack_test.lua
@@ -1,38 +1,13 @@
 local busted = require("busted")
 local assert = require("luassert")
 
-describe("Reference Test", function()
+describe("heap test", function()
     it("", function()
-        --stack test
-        local stack = {}
-    
-        assert.is_true(#stack == 0)
-        
-        table.insert(stack, "1")
-        table.insert(stack, "2")
-        
-        assert.is_true(#stack == 2)
-        
-        local removedItem = table.remove(stack)
-        
-        assert.is_true(removedItem == "2")
-        
-        assert.is_true(#stack == 1)  
-        
-        -- heap test
-        local heap = {}
-        
-        assert.is_true(next(heap) == nil)
-        
-        heap["key1"] = "1"
-        heap["key2"] = "2"
-        
-        assert.is_true(heap["key1"] == "1")
-        assert.is_true(heap["key2"] == "2")
-        
-        heap["key1"] = nil
-        
-        assert(heap["key1"] == nil)
-        
+       local heap = 9
+       heap = nil
+       local memory = collectgarbage('count')
+       collectgarbage()
+       
+       assert.is_true(collectgarbage('count') < memory)
     end)
 end)
